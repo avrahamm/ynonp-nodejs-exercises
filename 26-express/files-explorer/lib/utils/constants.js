dc176f6000a96d42a191f2979833b976ad2b277f
avrahamm <papaabramg@gmail.com>
2021-06-24 20:38:53 +0300

    making order

--- 26-express/files-explorer/lib/utils/constants.js
+++
@@ -1,5 +0,0 @@
-// const dirPath = 'D:\\YnonPerek\\nodejs\\nodejs-course-demos\\26-express-ex\\root-folder';
-const rootPath = '../root-folder/'; // from where the app is called  - project root
-module.exports = {
-    rootPath
-}
